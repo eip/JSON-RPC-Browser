6fa5e15c4298bbd14ca1468e14af29ab2cf63e4a
eip <eip@users.noreply.github.com>
2016-06-10 19:38:23 +0300

    Auto connect to Kodi host. Version 0.9.1.

--- js/main.js
+++ js/main.js
@@ -5,15 +5,23 @@ jQuery(document).ready(function ($) {
 	Renderer.initNavMenu();
 
     /** 
-        Case insensative search
+        Case insensitive search
         Source: http://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
      **/
+  
     $.expr[":"].contains = $.expr.createPseudo(function(arg) {
         return function( elem ) {
             return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
         };
     });
 	
+    if (location.protocol.indexOf('http') >= 0) { // Auto connect
+        $('#name').val(location.hostname);
+        $('#ajaxEndPoint').val(location.origin);
+        $('#socketEndPoint').val('ws://' + location.hostname + ':9090');
+        Renderer.loadSourceFromNewForm();
+    }
+
     //TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
     //Auto connect using the connection's form default values
     //Renderer.loadSourceFromNewForm();
